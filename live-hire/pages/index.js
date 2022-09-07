b4f69bbb8548364989b5310a4ca3e905ae210a71
hasan <hasanelmi678@gmail.com>
2022-09-07 05:41:19 +0100

    backgroundcolor

--- live-hire/pages/index.js
+++ live-hire/pages/index.js
@@ -5,15 +5,18 @@ import { useEffect, useState } from "react";
 import { db } from "../firebaseConfig";
 import Hero from "../components/hero";
 import JobsList from "../components/jobsList";
+import Loading from "../components/loading";
 
 
 const Home = () => {
   const [allJobs, setAllJobs] = useState([]);
   const [displayJobs, setDisplayJobs] = useState([]);
+  const [loadingJobs, setLoadingJobs] = useState(false);
   const { user } = useAuth();
 
   useEffect(() => {
     const getAllJobsFromFirebase = async () => {
+      setLoadingJobs(true)
       const q = query(collection(db, "jobs"));
 
       const querySnapshot = await getDocs(q);
@@ -25,6 +28,7 @@ const Home = () => {
       });
       setAllJobs(tempArr);
       setDisplayJobs(tempArr);
+      setLoadingJobs(false);
     }
 
     getAllJobsFromFirebase()
@@ -39,9 +43,9 @@ const Home = () => {
   return (
     <div className="home">
       <Header />
-      {displayJobs.length && <Hero displayJobs={displayJobs} setDisplayJobs={setDisplayJobs} allJobs={allJobs} /> }
-      {displayJobs.length ? <JobsList allJobs={displayJobs} /> : (
-        <h2>Loading...</h2>
+      {!loadingJobs && <Hero displayJobs={displayJobs} setDisplayJobs={setDisplayJobs} allJobs={allJobs} /> }
+      {!loadingJobs ? <JobsList allJobs={displayJobs} /> : (
+        <Loading />
       )}
       
     </div>
