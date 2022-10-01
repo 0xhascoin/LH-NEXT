38769eb59657ebe8b0c2afbb147845d00a1747fc
hasan <hasanelmi678@gmail.com>
2022-10-01 19:31:02 +0100

    error icon added

--- live-hire/components/accountErrorModal.jsx
+++ live-hire/components/accountErrorModal.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
-const AccountErrorModal = ({ setErrorMessage, errorMessage, setShowAccountErrorModal, showAccountErrorModal}) => {
-    
+const AccountErrorModal = ({ setErrorMessage, errorMessage, setShowAccountErrorModal, showAccountErrorModal }) => {
+
     const closeModal = () => {
         setErrorMessage(null)
         setShowAccountErrorModal(false);
     }
     return (
-        <div className={showAccountErrorModal ? "modal account-error is-active": "modal account-error"}>
+        <div className={showAccountErrorModal ? "modal account-error is-active" : "modal account-error"}>
             <div className="modal-background" onClick={closeModal}></div>
             <div className="modal-card">
                 <header className="modal-card-head">
                     <button className="delete" aria-label="close" onClick={closeModal}></button>
                 </header>
                 <section className="modal-card-body">
+                    <div className="icon">
+                        <AiOutlineCloseSquare />
+                    </div>
                     <h2 className="message">{errorMessage}</h2>
                 </section>
             </div>
